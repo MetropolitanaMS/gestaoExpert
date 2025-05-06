d44bda24139b32f1bcf0246d9b0bc3dc92efd627
Matheus Oliveira <matheussilvaoliveira@hotmail.com>
2025-05-06 15:52:17 -0400

    fix: resolving merge suggestions

--- components/SectionContact.vue
+++ components/SectionContact.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="py-28 relative">
+  <div id="contact" class="py-28 relative">
     <div class="flex flex-wrap lg:flex-nowrap mx-4 xl:mx-28 px-3.5">
       <div class="flex flex-col items-center px-3.5">
         <div
           class="flex flex-col w-full items-center rounded-md bg-secondary p-9 mr-5"
         >
           <div
-            class="rounded-full shadow-inner bg-[#353266] border-2 border-dashed border-amber-50 p-4 inline-flex items-center justify-center mb-6"
+            class="rounded-full shadow-inner bg-[#353266] border-1 border-dashed border-[#ffffff80] p-6 inline-flex items-center justify-center mb-6"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
               fill="currentColor"
-              class="w-17 h-17 text-white"
+              class="w-8 h-8 text-white"
             >
               <path
                 d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z"
